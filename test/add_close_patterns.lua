24bde18ea11180ba7e01f295074859a26185d534
Yuriy Artemyev <anuvyklack@gmail.com>
2022-04-13 10:24:28 +0300

    Add tests

---
+++ test/add_close_patterns.lua
@@ -0,0 +1,25 @@
+local M = {}
+
+function M.telescope() --{{{
+
+end --}}}
+
+-- use { 'vim-ctrlspace/vim-ctrlspace', --{{{
+
+-- } --}}}
+
+   -- use { 'vim-ctrlspace/vim-ctrlspace', --{{{
+   --    config = function ()
+   --       vim.g.CtrlSpaceDefaultMappingKey = "<C-space> "
+   --    end
+   -- } --}}}
+
+--{{{1 description
+
+-- }}}1
+
+-- {{{1 description
+
+-- }}}1
+
+-- vim: fdm=marker fml=1
