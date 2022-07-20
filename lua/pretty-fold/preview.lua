a7d8b424abe0eedf50116c460fbe6dfd5783b1d5
Yuriy Artemyev <anuvyklack@gmail.com>
2022-07-20 18:15:26 +0300

    fix: Add Void

--- lua/pretty-fold/preview.lua
+++ lua/pretty-fold/preview.lua
@@ -1,3 +1,14 @@
 local warn = require("pretty-fold.util").warn
 
 warn('preview module have been moved into separate plugin:: https://github.com/anuvyklack/fold-preview.nvim')
+
+---@class Void Void has eveything and nothing
+
+---@type Void
+local void = setmetatable({}, {
+   __index = function(self) return self end,
+   __newindex = function() end,
+   __call = function() end
+})
+
+return void
